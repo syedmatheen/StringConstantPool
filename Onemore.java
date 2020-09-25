7a1ec0c88384a45ea4501dd729acd3e945762442
syed <syedmatheen55@gmail.com>
2020-09-25 18:39:02 +0530

    Add files via upload

---
+++ Onemore.java
@@ -0,0 +1,14 @@
+public class Onemore {
+	 public static void main(String[] args) 
+	    { 
+	        String s1 = new String("abc"); 
+	        String s2 = new String("abc"); 
+	  
+	        // Note that this == compares whether 
+	        // s1 and s2 refer to same object or not 
+	        if (s1 == s2) 
+	           System.out.println("Yes"); 
+	        else
+	           System.out.println("No"); 
+	    } 
+}
---
+++ Sample.java
@@ -0,0 +1,14 @@
+public class Sample {
+	    public static void main(String[] args) 
+	    { 
+	        String s1 = "abc"; 
+	        String s2 = "abc"; 
+	  
+	        // Note that this == compares whether 
+	        // s1 and s2 refer to same object or not 
+	        if (s1 == s2) 
+	           System.out.println("Yes"); 
+	        else
+	           System.out.println("No"); 
+	    } 
+}
